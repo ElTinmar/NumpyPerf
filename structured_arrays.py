7aaedeee09ef9a1386b7e32c4355590947dece54
ElTinmar <martin.privat@hotmail.fr>
2024-04-17 18:20:33 +0200

    update

--- structured_arrays.py
+++ structured_arrays.py
@@ -2,17 +2,20 @@ import timeit
 
 setup_code = """
 import numpy as np
-from numpy.lib import recfunctions as rfn
 
-SZ = (1800, 1800)
+SZ = (4096, 4096)
 array = np.random.uniform(0, 1, SZ).astype(np.float32)
 dt = np.dtype([
     ('index', int, (1,)),
     ('timestamp', float, (1,)), 
     ('image', np.float32, SZ),
 ])
-struct = np.array((0, 1.00, array), dtype=dt)
-unstruct = rfn.structured_to_unstructured(struct).astype(np.float32)
+struct = np.array((0, 1.00, array), dtype=dt) # array is copied
+zero = np.zeros((1,), dtype=dt)
+"""
+
+assignment = """
+image = array
 """
 
 regular_copy = """
@@ -27,12 +30,26 @@ struct_array = """
 structured_image = np.array((0, 1.00, array), dtype=dt)
 """
 
-unstruct_to_struct = """
-structured_image2 = rfn.unstructured_to_structured(unstruct, dtype=dt)
+field_assignment = """
+zero['index'] = 0
+zero['timestamp'] = 1.0
+zero['image'] = array
 """
 
+tup_assignment = """
+zero[0] = (0, 1.0, array)
+"""
 
-timeit.repeat(setup=setup_code, stmt=regular_copy, repeat=3, number=1000)
-timeit.repeat(setup=setup_code, stmt=unstruct_array, repeat=3, number=1000)
-timeit.repeat(setup=setup_code, stmt=struct_array, repeat=3, number=1000)
-timeit.repeat(setup=setup_code, stmt=unstruct_to_struct, repeat=3, number=1000)
+print(assignment, timeit.repeat(setup=setup_code, stmt=assignment, repeat=3, number=1000))
+print(regular_copy, timeit.repeat(setup=setup_code, stmt=regular_copy, repeat=3, number=1000))
+print(unstruct_array, timeit.repeat(setup=setup_code, stmt=unstruct_array, repeat=3, number=1000))
+print(struct_array, timeit.repeat(setup=setup_code, stmt=struct_array, repeat=3, number=1000))
+print(field_assignment, timeit.repeat(setup=setup_code, stmt=field_assignment, repeat=3, number=1000))
+print(tup_assignment, timeit.repeat(setup=setup_code, stmt=field_assignment, repeat=3, number=1000))
+
+exec(setup_code)
+
+# checking modification of array
+np.allclose(struct['image'],array) # -> returns True
+array[0:100] = 0
+np.allclose(struct['image'],array) # -> returns False
